a7124cef0cd1e635016b860e765a73e2675a87ca
Joel Warburton <Joel.s.warburton@gmail.com>
2024-04-30 00:11:05 +1000

    Add eventListener and handler to cancel button. Update book list renderer

--- script.js
+++ script.js
@@ -24,6 +24,11 @@ submitBook.addEventListener("click", () => {
   }
 });
 
+const cancel = document.querySelector("#cancel");
+cancel.addEventListener("click", () => {
+  document.querySelector("#add-book-modal").close();
+});
+
 function checkForm(title, author, pageCount) {
   if (title != "" && author != "" && pageCount != "") {
     return true;
@@ -53,24 +58,28 @@ for (let i = 0; i < 3; i++) {
 
 function displayBookList() {
   const output = document.querySelector(".book-display");
+  const titleCard = createHeader();
   output.textContent = "";
   let index = 0;
+
+  output.appendChild(titleCard);
   myLibrary.forEach((book) => {
     //card for the book info
     const card = document.createElement("div");
     card.classList.add("card");
     card.setAttribute("data-index", index);
     const titleLabel = document.createElement("p");
-    titleLabel.textContent = "Title: " + book.title;
+    titleLabel.textContent = book.title;
     const authorLabel = document.createElement("p");
-    authorLabel.textContent = "Author: " + book.author;
+    authorLabel.textContent = book.author;
     const pagesLabel = document.createElement("p");
-    pagesLabel.textContent = "Pages: " + book.noPages;
+    pagesLabel.textContent = book.noPages;
 
     const readStatus = document.createElement("p");
-    readStatus.textContent = book.read;
+    readStatus.textContent = book.read === true ? "Read" : "Not Read";
 
     const readBtn = document.createElement("button");
+    readBtn.classList.add("read-button");
     readBtn.textContent = book.read ? "Mark Unread" : "Mark Read";
 
     readBtn.addEventListener("click", () => {
@@ -80,6 +89,7 @@ function displayBookList() {
     });
 
     const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-button");
     deleteBtn.textContent = "Delete";
     deleteBtn.addEventListener("click", (e) => {
       if (confirm("You are about to delete a book!\nAre you sure?") == true) {
@@ -89,16 +99,46 @@ function displayBookList() {
       }
     });
 
+    const buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("card-buttons");
+
     card.appendChild(titleLabel);
     card.appendChild(authorLabel);
     card.appendChild(pagesLabel);
     card.appendChild(readStatus);
-    card.appendChild(readBtn);
-    card.appendChild(deleteBtn);
+    buttonDiv.appendChild(readBtn);
+    buttonDiv.appendChild(deleteBtn);
+    card.appendChild(buttonDiv);
     output.appendChild(card);
 
     index++;
   });
 }
 
+function createHeader() {
+  let aCard = document.createElement("div");
+  aCard.classList.add("card");
+  aCard.classList.add("title-header");
+
+  const titleLabel = document.createElement("p");
+  titleLabel.textContent = "Title";
+  const authorLabel = document.createElement("p");
+  authorLabel.textContent = "Author";
+  const pagesLabel = document.createElement("p");
+  pagesLabel.textContent = "Number of Pages";
+  const readLabel = document.createElement("p");
+  readLabel.textContent = "Read Status";
+
+  const readBtn = document.createElement("button");
+  readBtn.textContent = "Mark Read";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  aCard.appendChild(titleLabel);
+  aCard.appendChild(authorLabel);
+  aCard.appendChild(pagesLabel);
+  aCard.appendChild(readLabel);
+
+  return aCard;
+}
 displayBookList();
